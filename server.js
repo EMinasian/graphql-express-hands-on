838649660811d4e2a6fe306c7ac095f40e07f2d0
erik.minasian <erik.minasian@coliquio.de>
2024-05-31 19:51:06 +0400

    feat: first test schema

--- server.js
+++ server.js
@@ -1,5 +1,27 @@
 const express = require("express");
+const expressGraphQL = require('express-graphql').graphqlHTTP
 const app = express();
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "test",
+    fields: () => ({
+      message: {
+        type: GraphQLString,
+        resolve: () => "returned test",
+      },
+    }),
+  }),
+});
+
+app.use(
+  "/graphql",
+  expressGraphQL({
+    schema: schema,
+    graphiql: true,
+  })
+);
 
 app.listen(5000, () => {
   console.log("Server running on port 5000!");
