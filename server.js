cf34656e6ba99c71f57451aeaaaec98db4f6a252
erik.minasian <erik.minasian@coliquio.de>
2024-06-03 12:56:56 +0400

    feat: author type added

--- server.js
+++ server.js
@@ -11,6 +11,15 @@ const {
 } = require("graphql");
 const { authors, books } = require("./mocks/data.js");
 
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  description: "The author of the books",
+  fields: () => ({
+    id: { type: new GraphQLNonNull(GraphQLInt) },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+  }),
+});
+
 const BookType = new GraphQLObjectType({
   name: "Book",
   description: "A book written by an author",
@@ -19,11 +28,11 @@ const BookType = new GraphQLObjectType({
     name: { type: new GraphQLNonNull(GraphQLString) },
     authorId: { type: new GraphQLNonNull(GraphQLInt) },
     author: {
-      type: new GraphQLNonNull(GraphQLString),
+      type: AuthorType,
       resolve: (parentBook) => {
         return (bookAuthor = authors.find(
           ({ id }) => parentBook?.authorId === id
-        )?.name);
+        ));
       },
     },
   }),
